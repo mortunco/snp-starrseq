49062e10eaa1de2355c7260411171ebe08cfc9e2
mortunco <tmorova15@ku.edu.tr>
2021-07-06 15:26:03 -0700

    one bp fix, added 24bp barcodes to create-umi output

--- create-umi-directory-paired.py
+++ create-umi-directory-paired.py
@@ -10,6 +10,6 @@ while True:
 	#print(mate2)
 
 	if mate1[3] > mate2[3]:
-		print("{}:{}-{}{}\t{}".format(mate1[2],mate2[3],mate2[9][0:3],mate1[9][-3::],mate2[0]))
+		print("{}:{}-{}{}\t{}{}\t{}".format(mate1[2],mate2[3],mate2[9][0:3],mate1[9][-3::],mate2[9][0:12],mate1[9][-12::],mate2[0]))
 	else:
-		print("{}:{}-{}{}\t{}".format(mate1[2],mate1[3],mate1[9][0:3],mate2[9][-3::],mate1[0]))
+		print("{}:{}-{}{}\t{}{}\t{}".format(mate1[2],mate1[3],mate1[9][0:3],mate2[9][-3::],mate1[9][0:12],mate2[9][-12::],mate1[0]))
