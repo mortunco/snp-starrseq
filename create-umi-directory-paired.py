908a8b7e050dee1e82fad025e8ee8630086ed4a2
mortunco <tmorova15@ku.edu.tr>
2021-12-12 09:35:09 -0800

    bug fix that cause vis errors

--- create-umi-directory-paired.py
+++ create-umi-directory-paired.py
@@ -10,6 +10,12 @@ while True:
 	#print(mate2)
 
 	if mate1[3] > mate2[3]:
-		print("{}:{}-{}{}\t{}{}\t{}".format(mate1[2],mate2[3],mate2[9][0:3],mate1[9][-3::],mate2[9][0:12],mate1[9][-12::],mate2[0]))
+		#first attempt: index, read barcode, readname
+		#print("{}:{}-{}{}\t{}{}\t{}".format(mate1[2],mate2[3],mate2[9][0:3],mate1[9][-3::],mate2[9][0:12],mate1[9][-12::],mate2[0]))
+		#pretty: index, read barcode, readname
+		#print(f'{mate1[2]}:{mate2[3]}-{mate2[9][0:3]}{mate1[9][-3::]}\t{mate2[9][0:12]}{mate1[9][-12::]}\t{mate2[0]}')
+		#index, read barcode, readname, chr, start, end 
+		print(f'{mate1[2]}:{mate2[3]}-{mate2[9][0:3]}{mate1[9][-3::]}\t{mate2[9][0:12]}{mate1[9][-12::]}\t{mate2[0]}\t{mate2[2]}\t{int(mate2[3])-1}\t{int(mate2[3])+abs(int(mate2[8]))}')
 	else:
-		print("{}:{}-{}{}\t{}{}\t{}".format(mate1[2],mate1[3],mate1[9][0:3],mate2[9][-3::],mate1[9][0:12],mate2[9][-12::],mate1[0]))
+		#print("{}:{}-{}{}\t{}{}\t{}".format(mate1[2],mate1[3],mate1[9][0:3],mate2[9][-3::],mate1[9][0:12],mate2[9][-12::],mate1[0]))
+		print(f'{mate1[2]}:{mate1[3]}-{mate1[9][0:3]}{mate2[9][-3::]}\t{mate1[9][0:12]}{mate2[9][-12::]}\t{mate1[0]}\t{mate1[2]}\t{int(mate1[3])-1}\t{int(mate1[3])+abs(int(mate1[8]))}')
